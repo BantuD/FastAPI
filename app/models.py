d1926859d4b56feaf14318bd49aade2992dc42bd
BantuD <smarty.bantu@gmail.com>
2023-03-01 00:29:41 +0530

    Update post table model

--- app/models.py
+++ app/models.py
@@ -11,6 +11,7 @@ class Post(Base):
     content = Column(String,nullable=False)
     published = Column(Boolean,default=True,nullable=False)
     created_at = Column(TIMESTAMP(timezone=True),nullable=False,server_default=text('now()'))
+    owner_id = Column(Integer,ForeignKey("users.id",ondelete="CASCADE"),nullable=False)
 
 class User(Base):
     __tablename__ = "users"
