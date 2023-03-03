c21722382632ceaf146b7ebeccb0cd2213e1468d
BantuD <smarty.bantu@gmail.com>
2023-03-03 14:21:59 +0530

    Added limit Query Parameter

--- app/routers/post.py
+++ app/routers/post.py
@@ -10,9 +10,9 @@ router = APIRouter(
 )
 
 @router.get('/',response_model=List[schemas.Post])
-def get_posts(db: Session = Depends(get_db),user = Depends(oauth2.get_current_user)):
+def get_posts(db: Session = Depends(get_db),user = Depends(oauth2.get_current_user),limit:int=10):
     
-    posts = db.query(models.Post).all()
+    posts = db.query(models.Post).limit(limit).all()
 
     return posts
 
