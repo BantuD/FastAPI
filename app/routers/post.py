c1c5cd07f409665ee4dc8dc218b6d279daab8d8e
BantuD <smarty.bantu@gmail.com>
2023-03-03 15:26:49 +0530

    Added search functionality .router/post.py request: get('/')

--- app/routers/post.py
+++ app/routers/post.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter,HTTPException,status,Depends
 from sqlalchemy.orm import Session
 from .. import database,models,schemas
 from ..database import get_db
-from typing import List
+from typing import List,Optional
 from .. import oauth2
 router = APIRouter(
     prefix="/posts",
@@ -10,9 +10,10 @@ router = APIRouter(
 )
 
 @router.get('/',response_model=List[schemas.Post])
-def get_posts(db: Session = Depends(get_db),user = Depends(oauth2.get_current_user),limit:int=10,skip:int=0):
+def get_posts(db: Session = Depends(get_db),user = Depends(oauth2.get_current_user),
+              limit:int=10,skip:int=0,search: Optional[str]=""):
     
-    posts = db.query(models.Post).limit(limit).offset(skip).all()
+    posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
 
     return posts
 
